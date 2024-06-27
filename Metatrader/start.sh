bf9b1fc21530737b75433762e5bcc1b77f3feefa
ender <codeotter0201@gmail.com>
2024-06-28 02:26:38 +0800

    update

--- Metatrader/start.sh
+++ Metatrader/start.sh
@@ -97,13 +97,13 @@ fi
 # Install mt5linux library in Windows if not installed
 show_message "[6/7] Checking and installing mt5linux library in Windows if necessary"
 if ! is_wine_python_package_installed "mt5linux"; then
-    $wine_executable python -m pip install --no-cache-dir mt5linux --no-deps
+    $wine_executable python -m pip install --no-cache-dir mt5linux
 fi
 
 # Install mt5linux library in Linux if not installed
 show_message "[6/7] Checking and installing mt5linux library in Linux if necessary"
 if ! is_python_package_installed "mt5linux"; then
-    pip install --upgrade --no-cache-dir mt5linux --no-deps
+    pip install --upgrade --no-cache-dir mt5linux
 fi
 
 # Install pyxdg library in Linux if not installed
